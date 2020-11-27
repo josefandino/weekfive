38002cf4e3498437e4495ff1903ab73f932a01f7
shecho <shechovala@gmail.com>
2020-11-27 01:05:05 -0500

    added urls files innet text

--- apps/estudiantes/models.py
+++ apps/estudiantes/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 
+
 class Student(models.Model):
-   nombre = models.CharField('Nombres', max_length=120, blank=True, null=False)
-   apellidos = models.CharField('Apellidos', max_length=120, blank=True, null=False)
-   telefono = models.CharField('Telefono', max_length=20, blank=True, null=False)
-   email = models.EmailField('Email', max_length=120, blank=True, null=False)
-   activo = models.BooleanField(default=False)
+    nombre = models.CharField('Nombres', max_length=120, blank=True, null=False)
+    apellidos = models.CharField('Apellidos', max_length=120, blank=True, null=False)
+    telefono = models.CharField('Telefono', max_length=20, blank=True, null=False)
+    email = models.EmailField('Email', max_length=120, blank=True, null=False)
+    activo = models.BooleanField(default=False)
 
-   
+    class Meta:
+        ordering = ['nombre']
+        verbose_name = 'Student'
+        verbose_name_plural = 'Students'
 
-   class Meta:
-      ordering = ['nombre']
-      verbose_name = 'Student'
-      verbose_name_plural = 'Students'
-
-   def __str__(self):
-      return '{0},{1}'.format(self.apellidos, self.nombre)
+    def __str__(self):
+        return '{0},{1}'.format(self.apellidos, self.nombre)
