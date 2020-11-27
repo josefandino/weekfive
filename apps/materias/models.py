15ca290ce6a5fc04d8cc1e442355adcfcb3d2d12
shecho <shechovala@gmail.com>
2020-11-27 01:08:54 -0500

    ome changes on models

--- apps/materias/models.py
+++ apps/materias/models.py
@@ -1,17 +1,18 @@
 from django.core.validators import MinLengthValidator
 from django.db import models
 
+
 class Materia(models.Model):
-   code = models.CharField(max_length=20)
-   name = models.CharField(max_length=150, validators=[MinLengthValidator(4)])
-   creditos = models.IntegerField(blank=True, null=False, validators=[MinLengthValidator(2)])
-   activo = models.BooleanField(default=False)
-   description = models.CharField(max_length=200, blank=True, null=False)
+    code = models.CharField(max_length=20)
+    name = models.CharField(max_length=150, validators=[MinLengthValidator(4)])
+    creditos = models.IntegerField(blank=True, null=False, validators=[MinLengthValidator(2)])
+    activo = models.BooleanField(default=False)
+    description = models.CharField(max_length=200, blank=True, null=False)
 
-   class Meta:
-      ordering = ['code']
-      verbose_name = 'Materia'
-      verbose_name_plural = 'Materias'
+    class Meta:
+        ordering = ['code']
+        verbose_name = 'Materia'
+        verbose_name_plural = 'Materias'
 
-   def __str__(self):
-      return '{0},{1}'.format(self.apellidos, self.nombre)
+    def __str__(self):
+        return self.name
