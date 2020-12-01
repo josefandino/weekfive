c2d055752e6e2328c9ef2995b4027abec4441dba
shecho <shechovala@gmail.com>
2020-11-30 19:27:35 -0500

    ome changes on models

--- apps/estudiantes/urls.py
+++ apps/estudiantes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-   path('student/', StudentAPIView.as_view()),
-   path('lacosa/', StudentView.as_view(), name='manager'),
+   path('student/', student),
+   path('lacosa/', student, name='manager'),
 ]
